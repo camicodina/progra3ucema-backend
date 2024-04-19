788f3dbeaf53c86a042cabac16d9610b316baf14
camicodina <camy.codi@gmail.com>
2024-04-19 00:05:38 -0300

    Inicialización del proyecto - Spring initializr

---
+++ src/main/java/com/ucema/progra3ucemabackend/Progra3ucemaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ucema.progra3ucemabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progra3ucemaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Progra3ucemaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucema/progra3ucemabackend/Progra3ucemaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucema.progra3ucemabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Progra3ucemaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
