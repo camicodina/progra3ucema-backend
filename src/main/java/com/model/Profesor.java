a161fa62a3434a4c05b2d6768bc7d77b9396ab22
camicodina <camy.codi@gmail.com>
2024-05-30 10:06:25 -0300

    import fix

--- src/main/java/com/model/Profesor.java
+++ src/main/java/com/model/Profesor.java
@@ -1,9 +1,6 @@
 package com.model;
 
-import java.time.LocalDate;
-
 public class Profesor extends Usuario {
-    private LocalDate fechaIngreso;
 
     public Profesor(String username, String name, String email, String password) {
         super(username, name, email, password);
--- src/main/java/com/ucema/progra3ucemabackend/Progra3ucemaBackendApplication.java
+++ src/main/java/com/ucema/progra3ucemabackend/Progra3ucemaBackendApplication.java
@@ -1,4 +1,4 @@
-package src.main.java.com.ucema.progra3ucemabackend;
+package com.ucema.progra3ucemabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
